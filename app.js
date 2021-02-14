ccea0f21a17766f11d87db2c40e5f3e96a6882a5
shafiimam <shafi15-8072@diu.edu.bd>
2021-02-14 21:20:22 +0600

    commented

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// features added for bonus mark:
+// 1.button under every image to view the full high resolution image in a new tab.
+// 2.spinner added for data loading.
+// 3.if any image is not found than a error will popup in the window.
+// 4.a ui implemented for error handling.
+
+
+
 const imagesArea = document.querySelector('.images');
 const gallery = document.querySelector('.gallery');
 const galleryHeader = document.querySelector('.gallery-header');
